d29437fdf3ddc22a634c925a774dba287c6dbb24
Johntavious Barkley <jbarkley3@gatech.edu>
2015-02-13 12:48:30 -0500

    Remodified Registration

--- app/src/main/java/com/example/johntavious/shoppingwithfriends/RegistrationActivity.java
+++ app/src/main/java/com/example/johntavious/shoppingwithfriends/RegistrationActivity.java
@@ -55,25 +55,28 @@ public class RegistrationActivity extends ActionBarActivity {
      * and log him into the system
      * @param view the done button click
      */
-    /**TODO: also check Username uniqueness"**/
+    /**TODO: also check Username uniqueness, password filled out, and fix bug allowing empty names"**/
     public void onDoneClick(View view) {
         String name = ((EditText)findViewById(R.id.name_field)).getText().toString();
+        System.out.println("@@@@@@@@@@@@@@@@@@@@" + name + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + name.equals(""));
         String email = ((EditText)findViewById(R.id.email_field)).getText().toString();
         String password = ((EditText)findViewById(R.id.password_field)).getText().toString();
-        if (name == "") {
+        if (name.trim() == "") {
             EditText nameView = (EditText)findViewById(R.id.name_field);
             nameView.setError("This field is invalid or taken");
             nameView.requestFocus();
-        } else if (LoginActivity.emailValid(email)) {
-            User user = new User(name, email, password);
-            LoginActivity.addUser(user);
-            Intent intent = new Intent(this, WelcomeActivity.class);
-            intent.putExtra("user", user.getName());
-            startActivity(intent);
         } else {
-            EditText emailView = (EditText)findViewById(R.id.email_field);
-            emailView.setError(getString(R.string.email_taken));
-            emailView.requestFocus();
+            if (LoginActivity.emailValid(email)) {
+                User user = new User(name, email, password);
+                LoginActivity.addUser(user);
+                Intent intent = new Intent(this, WelcomeActivity.class);
+                intent.putExtra("user", user.getName());
+                startActivity(intent);
+            } else {
+                EditText emailView = (EditText) findViewById(R.id.email_field);
+                emailView.setError(getString(R.string.email_taken));
+                emailView.requestFocus();
+            }
         }
     }
 }
