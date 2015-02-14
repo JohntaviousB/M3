9b582f5ef985017e757fa1368e2ec1cdde7057a6
cjwashington4 <cwashington34@gatech.edu>
2015-02-14 17:24:21 -0500

    Added FriendList page. Still needs list.

--- app/src/main/java/com/example/johntavious/shoppingwithfriends/FriendList.java
+++ app/src/main/java/com/example/johntavious/shoppingwithfriends/FriendList.java
@@ -1,46 +1,34 @@
 package com.example.johntavious.shoppingwithfriends;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Button;
 
-public class WelcomeActivity extends ActionBarActivity {
+public class FriendList extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        setContentView(R.layout.activity_friend_list);
+
         Bundle extras = getIntent().getExtras();
         String userName = "";
         if (extras != null) {
-            userName = extras.getString("user");
+            userName = extras.getString("name");
         }
-        TextView welcomeText = (TextView)findViewById(R.id.welcome_text);
-        welcomeText.setText("Welcome, " + userName);
+        TextView header = (TextView) findViewById(R.id.friend_list_header_text);
+        header.setText(userName + "'s Friends");
 
-        // Friend's List
-        Button friend_list_button = (Button) findViewById(R.id.friend_list_button);
-        final Intent goToFriendList = new Intent(this, FriendList.class);
-        goToFriendList.putExtra("name", userName);
-        friend_list_button.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View view) {
-                        startActivity(goToFriendList);
-                    }
-                }
-        );
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_welcome, menu);
+        getMenuInflater().inflate(R.menu.menu_friend_list, menu);
         return true;
     }
 
@@ -57,9 +45,5 @@ public class WelcomeActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-    public void onSignOutClick(View view) {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
     }
 }
